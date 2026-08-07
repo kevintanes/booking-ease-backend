06f5aea54a22b42f48f2dbb6f32ddbf9129a542b
kevin tanes <kevin.tanes@gmail.com>
2026-08-07 23:48:29 +0700

    feat: add new api getServiceById

--- src/services/service.service.ts
+++ src/services/service.service.ts
@@ -1,11 +1,23 @@
 import prisma from "../config/prisma.js";
+import { AppError } from "../utils/app-error.js";
 
 export const getServices = async () => {
   const services = await prisma.service.findMany({
-    include: {
-      category: true,
-    },
+    include: { category: true },
   });
 
   return services;
+};
+
+export const getService = async (id: string) => {
+  const service = await prisma.service.findUnique({
+    where: { id: id },
+    include: { category: true },
+  });
+
+  if (!service) {
+    throw new AppError("Service not found", 404);
+  }
+
+  return service;
 };
