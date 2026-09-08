cc8fc67e842eb3547be7a5f46798e4fe661a1061
kevin tanes <kevin.tanes@gmail.com>
2026-09-08 21:10:32 +0700

    feat: add new api get admin/bookings and patch admin/bookings/:id/status

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,6 +1,12 @@
 import type { NextFunction, Request, Response } from "express";
-import { getAdminDashboard } from "../services/admin.service.js";
+import {
+  getAdminDashboard,
+  getAllAdminBookings,
+  patchBookingStatus,
+} from "../services/admin.service.js";
 import { successResponse } from "../helper/response.js";
+import { BookingStatus } from "@prisma/client";
+import { AppError } from "../utils/app-error.js";
 
 export const getDashboardStats = async (
   req: Request,
@@ -9,6 +15,54 @@ export const getDashboardStats = async (
 ) => {
   try {
     const result = await getAdminDashboard();
+    return successResponse(res, 200, "Success", result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAdminBookings = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { status, search } = req.query;
+
+    const page = parseInt(req.query.page as string) || 1;
+    const limit = parseInt(req.query.limit as string) || 10;
+
+    const result = await getAllAdminBookings({
+      page,
+      limit,
+      search: search as string,
+      status: status as BookingStatus,
+    });
+
+    return successResponse(res, 200, "Success", result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateBookingStatus = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    if (!status || !Object.values(BookingStatus).includes(status)) {
+      throw new AppError("Invalid Status", 400);
+    }
+
+    const result = await patchBookingStatus(
+      id as string,
+      status as BookingStatus,
+    );
+
     return successResponse(res, 200, "Success", result);
   } catch (error) {
     next(error);
--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import { getDashboardStats } from "../controllers/admin.controller.js";
+import {
+  getAdminBookings,
+  getDashboardStats,
+  updateBookingStatus,
+} from "../controllers/admin.controller.js";
 import { authenticate, authorize } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.use(authenticate, authorize("ADMIN"));
 router.get("/dashboard-stats", getDashboardStats);
+router.get("/bookings", getAdminBookings);
+router.patch("/bookings/:id/status", updateBookingStatus);
 
 export default router;
